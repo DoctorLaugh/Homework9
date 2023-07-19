b02ec4d72ad449cd1785549c4de5f1f9e6b937a3
Dmitriy <trayan@yandex.ru>
2023-07-20 02:24:16 +0300

    Task66  added

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+int SumNum(int start, int end)
+{
+    int sum = 0;
+    if(start == end)
+    {
+        return end;
+    }
+    sum = start + SumNum(start+1, end);
+    return sum;
+}
+Clear();
+Write("Введите первое число: ");
+int n = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int m = int.Parse(ReadLine());
+WriteLine(SumNum(n, m));
