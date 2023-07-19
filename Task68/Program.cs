ec90666b8c97b2aea6edbff91df0a550282e90b3
Dmitriy <trayan@yandex.ru>
2023-07-20 02:33:45 +0300

    Task68  added

---
+++ Task68/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+int AkkFun(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if (n != 0 && m == 0)
+    {
+        return AkkFun(n - 1, 1);
+    }
+    else
+    {
+        return AkkFun(n - 1, AkkFun(n, m - 1));
+    }
+}
+Clear();
+Write("Введите первое число: ");
+int n = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int m = int.Parse(ReadLine());
+WriteLine(AkkFun(n, m));
