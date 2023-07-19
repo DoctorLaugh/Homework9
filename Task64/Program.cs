f1a6bba803fe39fc2f24f7413b4a8ed1877d0071
Dmitriy <trayan@yandex.ru>
2023-07-20 02:03:29 +0300

    Task64  added

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+string RevNatRow(int n)
+{
+    if(n > 1)
+    {
+        Write($"{n}, ");
+        RevNatRow(n - 1);
+    }
+    return "1";
+}
+Clear();
+Write("Введите число: ");
+int n = int.Parse(ReadLine());
+WriteLine(RevNatRow(n));
